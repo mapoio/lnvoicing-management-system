acc5ab75bc5a8a2bb2e5172b7cb4415c8f2e373d
ZengJiean <zengjiean@huas.edu.gr>
2019-05-05 16:02:41 +0800

    feat: add basic

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -6,6 +6,7 @@ const Axios = axios.create({
   timeout: 1000,
   headers: {
     Authorization:
+      // tslint:disable-next-line: max-line-length
       'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTU3MDI4NTYyLCJleHAiOjE1NTk2MjA1NjJ9.BUbT1tMt1k_0z1fNaVPj3KjlMebkNOgfxfMznujN9_g'
   }
 });
